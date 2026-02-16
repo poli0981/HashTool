ae33bf7d4586011089751649f43a9011ef503f87
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-02-16 23:43:50 +0700

    feat: add AlgorithmEnabledConverter for dynamic ComboBox enabling based on processing state

---
+++ Converters/AlgorithmEnabledConverter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using CheckHash.Models;
+
+namespace CheckHash.Converters;
+
+public class AlgorithmEnabledConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Count != 2)
+            return false;
+
+        if (values[0] is not FileStatus status ||
+            values[1] is not bool isComputing)
+        {
+            return false;
+        }
+
+        if (!isComputing)
+        {
+            return true;
+        }
+
+        // If computing, disable for Ready (Queue) or Processing (Active)
+        // Enable for Success, Failure, Cancelled (Complete)
+        return status != FileStatus.Ready && status != FileStatus.Processing;
+    }
+}
